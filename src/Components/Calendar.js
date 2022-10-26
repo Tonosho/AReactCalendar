7110d49cdcf90c2fe3bb2b5996fb5c7513d8e7b7
Tonosho <102292299+Tonosho@users.noreply.github.com>
2022-10-26 18:01:42 +0200

    add ability to return to default month

---
+++ src/Components/Calendar.js
@@ -0,0 +1,95 @@
+import { useState } from 'react';
+
+import './Calendar.css';
+import { Days } from './Days.js';
+
+const weekDays = ["S", "M", "T", "W", "T", "F", "S"];
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+const todayDate = new Date();
+
+export const Calendar = () => {
+
+    const [selectedDate, setSelectedDate] = useState(new Date());
+    const selectedMonth = months[selectedDate.getMonth()]
+    const selectedYear = selectedDate.getFullYear();
+
+    const previousMonthClick = () => {
+        setSelectedDate(
+            new Date(
+                selectedDate.getFullYear(),
+                selectedDate.getMonth() - 1,
+                (selectedDate.getMonth() - 1) === todayDate.getMonth() && selectedDate.getFullYear() === todayDate.getFullYear() ? 
+                todayDate.getDate() : 1))
+    };
+
+    const nextMonthClick = () => {
+        setSelectedDate(
+            new Date(
+                selectedDate.getFullYear(),
+                selectedDate.getMonth() + 1,
+                (selectedDate.getMonth() + 1) === todayDate.getMonth() && selectedDate.getFullYear() === todayDate.getFullYear() ? 
+                todayDate.getDate() : 1))
+    };
+
+    return (
+        <div className="calendar">
+            <div className="calendar_header">
+                <h2>Calendar</h2>
+            </div>
+            {/* TODAY'S DATE */}
+            <div className="calendar_currentDate">
+                <p>Date :</p>
+                <p className="calendar_dateDisplay">{selectedDate.toLocaleDateString().replaceAll('/', ' / ')}</p>
+            </div>
+            {/* CALENDAR */}
+            <div className="calendar_body">
+                <div className="calendar_dateSelectContainer">
+                    <p className="calendar_monthAndYear">{selectedMonth} {selectedYear}</p>
+                    {/* Change months buttons */}
+                    <div className="calendar_arrowsContainer">
+                        <button
+                            className="calendar_previousMonth" onClick={() => previousMonthClick()}
+                        >
+                            {"<"}
+                        </button>
+                        <button
+                            className="calendar_nextMonth" onClick={() => nextMonthClick()}
+                        >
+                            {">"}
+                        </button>
+                    </div>
+                </div>
+                {/* Weeks header */}
+                <div className="calendar_weekdaysHeader">
+                    {weekDays.map((weekday, index) => {
+                        return (
+                            <div key={index}>{weekday}</div>
+                        )
+                    })}
+                </div>
+                {/* Days display */}
+                <Days
+                    selectedDate={selectedDate}
+                    onChangeSelectedDate={(date) => setSelectedDate(date)}
+                />
+
+                {/* SCHEDULE AND CANCEL BUTTONS */}
+                <div className="calendar_scheduleContainer">
+                    <button
+                        className="calendar_scheduleButton"
+                        onClick={() => alert(`Yous selected ${selectedDate.toLocaleDateString().replaceAll('/', ' / ')}`)}
+                    >
+                        Schedule
+                    </button>
+                    <button
+                        className="calendar_cancelButton"
+                        onClick={() => setSelectedDate(new Date())}
+                    >
+                        Cancel
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
---
+++ src/Components/Days.js
@@ -0,0 +1,61 @@
+import { useState } from 'react';
+import './Days.css';
+
+const CALENDAR_GRID = 42;
+// const grid = new Array(CALENDAR_GRID)
+
+export const Days = (props) => {
+
+  // Props
+  const { selectedDate, onChangeSelectedDate } = props;
+
+  const firstDayOfMonth = new Date(
+    selectedDate.getFullYear(),
+    selectedDate.getMonth(),
+    1
+  )
+
+  const weekdayOfFirstDay = firstDayOfMonth.getDay();
+  const displayedDays = [];
+
+
+  for (let cell =0; cell < CALENDAR_GRID; cell++) {
+    if (cell === 0 && weekdayOfFirstDay === 0) {
+      // Add top row in case first day of month is first day of week
+      firstDayOfMonth.setDate(firstDayOfMonth.getDate() - 7);
+    } else if (cell === 0) {
+      // Offset first day of month to correct day of the week
+      firstDayOfMonth.setDate(firstDayOfMonth.getDate() - weekdayOfFirstDay);
+    } else {
+      firstDayOfMonth.setDate(firstDayOfMonth.getDate() + 1);
+    }
+
+    const calendarDay = {
+      date: new Date(firstDayOfMonth),
+      number: firstDayOfMonth.getDate(),
+      selected: firstDayOfMonth.toLocaleDateString() === selectedDate.toLocaleDateString(),
+      excluded: firstDayOfMonth.getMonth() !== selectedDate.getMonth(),
+    }
+
+    displayedDays.push(calendarDay)
+  }
+
+  return (
+    <div className="days_container">
+      {displayedDays.map((day, index) => {
+        return (
+          <div
+            key={index}
+            className={`days_day 
+            ${day.selected ? "m-selected" : ""} 
+            ${day.excluded ? "m-excluded" : ""}`}
+            onClick={() => onChangeSelectedDate(day.date)}
+          >
+            {day.number}
+          </div>
+        )
+      })}
+    </div>
+  );
+
+};
